53c99f98a796cf5626332d244068f14e07fc9293
anton-lapin <lapin_antoine@mail.ru>
2022-08-04 22:40:03 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Petrov", "Java Junior Developer", LocalDateTime.now()));
+        candidates.put(2, new Candidate(2, "Ivanov", "Java Middle Developer", LocalDateTime.now()));
+        candidates.put(3, new Candidate(3, "Sidorov", "Java Senior Developer", LocalDateTime.now()));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
