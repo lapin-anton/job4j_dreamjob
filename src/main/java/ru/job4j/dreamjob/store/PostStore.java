2be0d5c88906b5fd74ed8a32404aa8e706998341
anton-lapin <lapin_antoine@mail.ru>
2022-08-04 22:04:38 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "Easy to work", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "Very easy to work", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "Best job on the Earth", LocalDateTime.now()));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
+}
